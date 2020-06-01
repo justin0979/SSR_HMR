fae11ab61bb5d6143a637375e5178b976448c689
justin0979 <justinm0979@gmail.com>
2020-06-01 00:05:49 -0500

    ran npm audit and update, add lazy loaded comp to App

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
-
-const hooks = ['Hooks', 'Are', 'Supported', 'with', 'react-hot-loader'];
+import React, { useState, Suspense, lazy } from "react";
+const Other = lazy(() => import("./Other"));
 
 const App = () => {
-  const [comments] = React.useState(hooks);
+  const hooks = [
+    "Hooks",
+    "Are",
+    "Supported",
+    "with",
+    "react-hot-loader",
+  ];
+  const base = ["Base", "setup", "with", "hooks"];
+  const [comments, setComments] = useState(base);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const showHookMsg = () => {
+    if (comments.length === 4) {
+      setComments(hooks);
+    } else {
+      setComments(base);
+    }
+  };
+
+  const lazyLoadOther = () => {
+    if (isLoaded) {
+      return (
+        <Suspense fallback="getting that lazy component">
+          <Other />
+        </Suspense>
+      );
+    }
+    return <h3>Nothing lazy loaded here.</h3>;
+  };
 
   return (
     <div className="app">
-      {comments.map(comment => (
+      {comments.map((comment) => (
         <h1 key={comment}>{comment}</h1>
       ))}
+      <button className="btn" onClick={showHookMsg}>
+        Show Other Message
+      </button>
+      <button className="btn" onClick={() => setIsLoaded(!isLoaded)}>
+        Lazy Load Component
+      </button>
+      {lazyLoadOther()}
     </div>
   );
 };
